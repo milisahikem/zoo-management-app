766073c65c1b61b76795041e05733c4efd9260d8
milisahikem <milisa.hikem@gmail.com>
2026-04-18 14:28:20 +0200

    Delete public_html directory

--- public_html/changer_mdp_force.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-require_once("auth.php");
-require_login();
-header("Location: changer_mdp.php");
-exit();
-?>
--- public_html/script.js
+++
@@ -1,9 +0,0 @@
-// Change le fond de la navbar au scroll
-window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
